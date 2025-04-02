fbdb9c1fcbb0cfdc5686c4584876828710be02c1
William Manning <william.manning@brainlab.com>
2025-04-02 14:58:53 -0400

    Finish part 6

--- Plugins/GOL_Demo/Source/GOL_Demo/Private/GOL_RenderPass.cpp
+++ Plugins/GOL_Demo/Source/GOL_Demo/Private/GOL_RenderPass.cpp
@@ -1,5 +1,6 @@
 #include "GOL_RenderPass.h"
 
+#include "MaterialCompiler.h"
 #include "RenderGraphUtils.h"
 #include "SnkePostProcessMaterialShaders.h"
 #include "Runtime/Renderer/Private/PostProcess/PostProcessing.h"
@@ -296,4 +297,74 @@ void F_GOL_PassSVE::PrePostProcessPass_RenderThread(FRDGBuilder& graph, const FS
 		passMaterial,
 		GetSceneTextureShaderParameters(inputs.SceneTextures)
 	);
+}
+
+int32 UMaterialExpressionGoLInitOutputs::Compile(FMaterialCompiler* compiler, int32 pinIdx)
+{
+	int32 codeID;
+	if (pinIdx == 0)
+	{
+		codeID = InitialBinaryState.IsConnected() ?
+				     InitialBinaryState.Compile(compiler) :
+					 INDEX_NONE;
+	}
+	else if (pinIdx == 1)
+	{
+		codeID = InitialContinuousState.IsConnected() ?
+				     InitialContinuousState.Compile(compiler) :
+					 INDEX_NONE;
+	}
+	else
+	{
+		codeID = INDEX_NONE;
+	}
+	return compiler->CustomOutput(this, pinIdx, codeID);
+}
+int32 UMaterialExpressionGoLSimulate1Outputs::Compile(FMaterialCompiler* compiler, int32 pinIdx)
+{
+	int32 codeID;
+	auto doPin = [&](int32 i, FExpressionInput& pin, float* fallback)
+	{
+		if (pinIdx != i)
+			return false;
+		
+		if (pin.IsConnected())
+			codeID = pin.Compile(compiler);
+		else if (fallback)
+			codeID = compiler->Constant(*fallback);
+		else
+			codeID = INDEX_NONE;
+		return true;
+	};
+
+	if (!doPin(0, ThresholdTooFew, &ThresholdTooFewConst) &&
+		!doPin(1, ThresholdResurrect, &ThresholdResurrectConst) &&
+		!doPin(2, ThresholdTooMany, &ThresholdTooManyConst))
+	{
+		codeID = INDEX_NONE;
+	}
+	return compiler->CustomOutput(this, pinIdx, codeID);
+}
+int32 UMaterialExpressionGoLSimulate2Outputs::Compile(FMaterialCompiler* compiler, int32 pinIdx)
+{
+	int32 codeID;
+	auto doPin = [&](int32 i, FExpressionInput& pin, float* fallback)
+	{
+		if (pinIdx != i)
+			return false;
+		
+		if (pin.IsConnected())
+			codeID = pin.Compile(compiler);
+		else if (fallback)
+			codeID = compiler->Constant(*fallback);
+		else
+			codeID = INDEX_NONE;
+		return true;
+	};
+
+	if (!doPin(0, ContinuousValue, nullptr))
+	{
+		codeID = INDEX_NONE;
+	}
+	return compiler->CustomOutput(this, pinIdx, codeID);
 }
--- Plugins/GOL_Demo/Source/GOL_Demo/Public/GOL_RenderPass.h
+++ Plugins/GOL_Demo/Source/GOL_Demo/Public/GOL_RenderPass.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Materials/MaterialExpressionCustomOutput.h"
 
 #include "SnkeCustomRenderPasses.h"
 
@@ -58,3 +59,86 @@ struct GOL_DEMO_API F_GOL_PassSVE : public TSnkeRenderPassSceneViewExtension<U_G
 	virtual void PrePostProcessPass_RenderThread(FRDGBuilder& graph, const FSceneView& view,
 												 const FPostProcessingInputs& inputs) override;
 };
+
+
+#pragma region Custom Material Outputs
+
+UCLASS(CollapseCategories, HideCategories=Object, DisplayName="GoL Outputs: Init")
+class GOL_DEMO_API UMaterialExpressionGoLInitOutputs : public UMaterialExpressionCustomOutput
+{
+	GENERATED_BODY()
+public:
+
+	//Automatically snapped to 0 or 1.
+	UPROPERTY(meta=(RequiredInput=true))
+	FExpressionInput InitialBinaryState;
+
+	//If not set, defaults to the binary state value (before it's snapped to 0 or 1).
+	UPROPERTY(meta=(RequiredInput=false))
+	FExpressionInput InitialContinuousState;
+	
+	virtual FString GetFunctionName() const override { return TEXT("GoL_Outputs_Init_"); }
+	virtual FString GetDisplayName() const override { return TEXT("GoL Outputs: Init"); }
+
+	#if WITH_EDITOR
+		virtual void GetCaption(TArray<FString>& output) const override { output.Add(TEXT("Game of Life Outputs: Init pass")); }
+		virtual int32 GetNumOutputs() const override { return 2; }
+		virtual EShaderFrequency GetShaderFrequency() override { return SF_Pixel; }
+		virtual int32 Compile(class FMaterialCompiler*, int32 pinIdx) override;
+	#endif
+};
+
+UCLASS(CollapseCategories, HideCategories=Object, DisplayName="GoL Outputs: Simulate (pt 1)")
+class GOL_DEMO_API UMaterialExpressionGoLSimulate1Outputs : public UMaterialExpressionCustomOutput
+{
+	GENERATED_BODY()
+public:
+
+	UPROPERTY(meta=(RequiredInput=false), DisplayName="Threshold: Underpopulated")
+	FExpressionInput ThresholdTooFew;
+	UPROPERTY(meta=(OverridingInputProperty=ThresholdTooFew))
+	float ThresholdTooFewConst = 2.0f;
+	
+	UPROPERTY(meta=(RequiredInput=false), DisplayName="Threshold: Ideal")
+	FExpressionInput ThresholdResurrect;
+	UPROPERTY(meta=(OverridingInputProperty=ThresholdResurrect))
+	float ThresholdResurrectConst = 2.5f;
+	
+	UPROPERTY(meta=(RequiredInput=false), DisplayName="Threshold: Overpopulation")
+	FExpressionInput ThresholdTooMany;
+	UPROPERTY(meta=(OverridingInputProperty=ThresholdTooMany))
+	float ThresholdTooManyConst = 3.0f;
+	
+	virtual FString GetFunctionName() const override { return TEXT("GoL_Outputs_Simulate_Pt1_"); }
+	virtual FString GetDisplayName() const override { return TEXT("GoL Outputs: Simulate (part 1)"); }
+
+	#if WITH_EDITOR
+		virtual void GetCaption(TArray<FString>& output) const override { output.Add(TEXT("Game of Life Outputs: Simulate pass (part 1)")); }
+		virtual int32 GetNumOutputs() const override { return 3; }
+		virtual EShaderFrequency GetShaderFrequency() override { return SF_Compute; }
+		virtual int32 Compile(class FMaterialCompiler*, int32 pinIdx) override;
+	#endif
+};
+
+UCLASS(CollapseCategories, HideCategories=Object, DisplayName="GoL Outputs: Simulate (pt 2)")
+class GOL_DEMO_API UMaterialExpressionGoLSimulate2Outputs : public UMaterialExpressionCustomOutput
+{
+	GENERATED_BODY()
+public:
+
+	//If not provided, the continuous state is set to match the discrete one.
+	UPROPERTY(meta=(RequiredInput=false))
+	FExpressionInput ContinuousValue;
+	
+	virtual FString GetFunctionName() const override { return TEXT("GoL_Outputs_Simulate_Pt2_"); }
+	virtual FString GetDisplayName() const override { return TEXT("GoL Outputs: Simulate (part 2)"); }
+
+#if WITH_EDITOR
+	virtual void GetCaption(TArray<FString>& output) const override { output.Add(TEXT("Game of Life Outputs: Simulate pass (part 2)")); }
+	virtual int32 GetNumOutputs() const override { return 1; }
+	virtual EShaderFrequency GetShaderFrequency() override { return SF_Compute; }
+	virtual int32 Compile(class FMaterialCompiler*, int32 pinIdx) override;
+#endif
+};
+
+#pragma endregion
